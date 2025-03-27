11b93f93dd0549e6f86471f32fa91abb3f512d77
max_tonny8 <tonnyjansen0831@gmail.com>
2025-03-28 05:16:27 +0900

    Update wrapSol.ts

--- src/wrapSol.ts
+++ src/wrapSol.ts
@@ -8,37 +8,7 @@ import { sleep } from "../utils";
  * @param {number} wsolAmount - The amount of SOL to wrap.
  */
 export const wrapSol = async (connection: Connection, mainKp: Keypair, wsolAmount: number) => {
-  try {
-    const wSolAccount = await getAssociatedTokenAddress(NATIVE_MINT, mainKp.publicKey);
-    const tx = new Transaction().add(
-      ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 500_000 }),
-      ComputeBudgetProgram.setComputeUnitLimit({ units: 51_337 }),
-    );
-    if (!await connection.getAccountInfo(wSolAccount))
-      tx.add(
-        createAssociatedTokenAccountIdempotentInstruction(
-          mainKp,
-          wSolAccount,
-          mainKp.publicKey,
-          NATIVE_MINT,
-        ),
-        SystemProgram.transfer({
-          fromPubkey: mainKp.publicKey,
-          toPubkey: wSolAccount,
-          lamports: Math.floor(wsolAmount * 10 ** 9),
-        }),
-        createSyncNativeInstruction(wSolAccount)
-      )
-   
-    tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
-    tx.feePayer = mainKp
-    // console.log("Wrap simulation: ", await connection.simulateTransaction(tx))
-    const sig = await sendAndConfirmTransaction(connection, tx, [mainKp], { skipPreflight: true, commitment: "confirmed" });
-    console.log(`Wrapped SOL transaction: https://solscan.io/tx/${sig}`);
-    await sleep(5000);
-  } catch (error) {
-    // console.error("wrapSol error:", error);
-  }
+  
 };
 
 /**
@@ -46,26 +16,5 @@ export const wrapSol = async (connection: Connection, mainKp: Keypair, wsolAmoun
  * @param {Keypair} mainKp - The main keypair.
  */
 export const unwrapSol = async (connection: Connection, mainKp: Keypair) => {
-  const wSolAccount = await getAssociatedTokenAddress(NATIVE_MINT, mainKp.publicKey);
-  try {
-    const wsolAccountInfo = await connection.getAccountInfo(wSolAccount);
-    if (wsolAccountInfo) {
-      const tx = new Transaction().add(
-        ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 500_000 }),
-        ComputeBudgetProgram.setComputeUnitLimit({ units: 101337 }),
-        createCloseAccountInstruction(
-          wSolAccount,
-          mainKp,
-          mainKp.publicKey,
-        ),
-      );
-      tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
-      tx.feePayer = mainKp
-      const sig = await sendAndConfirmTransaction(connection, tx, [mainKp], { skipPreflight: true, commitment: "confirmed" });
-      console.log(`Unwrapped SOL transaction: https://solscan.io/tx/${sig}`);
-      await sleep(5000);
-    }
-  } catch (error) {
-    // console.error("unwrapSol error:", error);
-  }
+  
 };
